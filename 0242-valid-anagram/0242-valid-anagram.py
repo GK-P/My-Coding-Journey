ce98a9232a0872967414e54bb08714636d202c16
GK-P <155941869+GK-P@users.noreply.github.com>
2026-07-30 22:22:51 +0530

    Time: 15 ms (44.45%), Space: 19.4 MB (75.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,5 +3,24 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        return sorted(s) == sorted(t)
+        count_list = {}
+
+        for c in s:
+            if c in count_list:
+                count_list[c] += 1
+            else:
+                count_list[c] = 1
+
+        for c in t:
+            if c not in count_list:
+                return False
+            
+            count_list[c] -= 1
+
+            if count_list[c] < 0:
+                return False
+    
+        return True
+              
+
         
